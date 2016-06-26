36a86ccacea932da5e54884c591da6ef4378bc80
Marco Hartmann <marco.hartmann@tuhh.de>
2016-06-26 14:31:10 +0200

    remove unecessary brackets

--- ComplexityAlgorithms.cpp
+++ ComplexityAlgorithms.cpp
@@ -9,15 +9,15 @@ bool ComplexityAlgorithms::randomWalk(satFormula *S, int t)
 {
     for(int u=0; u<t; u++)
     {
-        for(int i=0; i<(S->n); i++)
+        for(int i=0; i<S->n; i++)
         {
             if( rand() % 2 )
             {
-                (S->xVal)[i] = true;
+                S->xVal[i] = true;
             }
         }
 
-        for(int v=0; v<3*(S->n); v++)
+        for(int v=0; v<3*S->n; v++)
         {
             if(satisfied(S))
             { return true; }
@@ -30,7 +30,7 @@ bool ComplexityAlgorithms::randomWalk(satFormula *S, int t)
             i = chooseI(S, l);
 
             //flip the value of the i-th variable xi
-            (S->xVal)[i] = ~(S->xVal)[i];
+            S->xVal[i] = ~S->xVal[i];
         }
     }
 
@@ -40,7 +40,7 @@ bool ComplexityAlgorithms::randomWalk(satFormula *S, int t)
 //check whether or not k-SAT formula with m clauses is satisfied or not
 bool ComplexityAlgorithms::satisfied(struct satFormula *S) const
 {
-    for(int i=0, m=(S->m); i<m; i++)
+    for(int i=0, m=S->m; i<m; i++)
     {
         //the k-SAT formula is true iff all of its clauses evaluate to true
         if(!checkClause(S, i))
@@ -53,11 +53,11 @@ bool ComplexityAlgorithms::satisfied(struct satFormula *S) const
 //check whether or not the i-th Clause of the k-SAT formula evaluates to true
 bool ComplexityAlgorithms::checkClause(struct satFormula *S, int i) const
 {
-    for(int j=0; j<(S->k); j++)
+    for(int j=0; j<S->k; j++)
     {
         //combine the sign of xi with its value
         //the whole clause is true iff one of the literals evaluates to true
-        if((S->xData)[i][j].second ^ S->xVal[ (S->xData)[i][j].first ])
+        if(S->xData[i][j].second ^ S->xVal[ S->xData[i][j].first ])
         { return true; }
     }
 
@@ -82,6 +82,6 @@ int ComplexityAlgorithms::chooseL(struct satFormula *S) const
 //randomly choose an index, which is the index (name) of a literal in the not fulfilled clause
 int ComplexityAlgorithms::chooseI(struct satFormula *S, int l) const
 {
-    int r = rand() % (S->k);
-    return (S->xData)[l][r].first;
+    int r = rand() % S->k;
+    return S->xData[l][r].first;
 }
